17d95412b2d9ed85aaa834837c43fc4c4e8eb0cb
tezumozu <kadomiumucd48@gmail.com>
2020-06-10 20:06:56 +0900

    Script_testを追加このフォルダはコードの練習に使用

---
+++ Script_Test/Test_UniRx/Assets/CubeScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Subject<Unit> OnPressedSpace = new Subject<Unit>();
+    private Subject<Unit> OnPressedSpace2 = new Subject<Unit>();
+    void Start()
+    {
+        StartCoroutine(testCoroutine());
+
+        OnPressedSpace.Subscribe( x => {
+            Debug.Log(x);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator testCoroutine(){
+        while(true){
+            if(Input.GetKey(KeyCode.Space)){
+                break;
+            }
+            yield return null; 
+        }
+
+        OnPressedSpace.OnNext(Unit.Default);
+        yield return null; 
+    }
+}
